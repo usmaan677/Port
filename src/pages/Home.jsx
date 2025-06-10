9d4ddac5f087854a4b1fc93a4cb4e1651dfa02f2
Usmaan Sayed <161072079+usmaan677@users.noreply.github.com>
2025-06-10 14:10:12 -0500

    border on image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = ({theme}) => {
     <div className={`${theme === "dark" ? "bg-gray-900 text-white" : "bg-gray-200 text-gray-800"} min-h-screen`}>
         <section className = "flex items-center justify-center gap-1 md:gap-30 p-10 max-w-6xl mx-auto flex-col md:flex-row text-center md:text-left mb-16">
             <div className = "w-60 h-60 md:w-80 md:h-80 rounded-full overflow-hidden shadow-md self-center ">
-                <img src = {myPhoto} alt  ="Usmaan Sayed" className = "object-cover w-full h-full "/>
+                <img src = {myPhoto} alt  ="Usmaan Sayed" className = "border-2 border-white rounded-full object-cover w-full h-full "/>
             </div>
             <div className = "text-3xl md:text-5xl font-extrabold max-w-md break-words leading-tight self-center ">
                 <div style={{  }}>
