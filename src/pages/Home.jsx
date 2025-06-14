2d784c61c9bb9e9f32b1a853340e0da8feb393cd
Usmaan Sayed <161072079+usmaan677@users.noreply.github.com>
2025-06-14 15:14:32 -0500

    minor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = ({theme, experienceRef}) => {
   
   return (
     <div className={`${theme === "dark" ? "bg-gray-800 text-white" : "bg-gray-200 text-gray-800"} min-h-screen`}>
-        <section className = "flex items-center justify-center gap-1 md:gap-30 p-10 max-w-6xl mx-auto flex-col md:flex-row text-center md:text-left mb-16 ">
+        <section className = "flex items-center justify-center gap-1 md:gap-30 p-10 max-w-6xl mx-auto flex-col md:flex-row text-center md:text-left mb-5 ">
             <div className = "w-60 h-60 md:w-100 md:h-100 rounded-full overflow-hidden shadow-md self-center ">
                 <img src = {myPhoto} alt  ="Usmaan Sayed" className = "border-2 border-white rounded-full object-cover w-full h-full "/>
             </div>
@@ -24,7 +24,7 @@ const Home = ({theme, experienceRef}) => {
                 <div style={{  }}>
                     <DecryptedText
                         words={['Usmaan Sayed', 'Software Engineer', 'Creative Thinker']}
-                        className="text-6xl font-bold"
+                        className="text-4xl pt-5 md:pt-0 md:text-6xl font-bold"
                         animateOn="hover"
                         />
                     {/* <DecryptedText
@@ -41,7 +41,7 @@ const Home = ({theme, experienceRef}) => {
         <section>
             <div className = 'pb-20'>
                 <h2 className=' px-10 md:px-40 pb-10 text-4xl md:text-4xl '>About Me</h2>
-                <p className = ' mx-10 md:mx-40 md:text-2xl md:mr-[50vh]'>Hey! I'm Usmaan Sayed, a passionate student majoring in computer science always interested in learning new technologies and building new projects.
+                <p className = ' mx-10 md:mx-40 text-xl md:text-2xl md:mr-[50vh]'>Hey! I'm Usmaan Sayed, a passionate student majoring in computer science always interested in learning new technologies and building new projects.
                     However, there is also much more to me than just coding. I love basketball, lego, robotics, and spedning time with friends!
                 </p>
             </div>
